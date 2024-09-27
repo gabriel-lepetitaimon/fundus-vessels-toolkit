eaf2e4bcaa804275c443fa0f65e1029e8be53e06
gabriel.lepetitaimon <gabriel.lepetitaimon@gmail.com>
2024-09-27 17:32:11 -0400

    Adding interactive view of biomarkers (Double click!)

--- src/fundus_vessels_toolkit/pipelines/av_to_biomarkers.py
+++ src/fundus_vessels_toolkit/pipelines/av_to_biomarkers.py
@@ -52,15 +52,27 @@ class FundusAVSegToBiomarkers:
         }
 
     def inspect_bifurcations(self, fundus_data: FundusData, *, show_tangents=True):
-        import plotly.graph_objects as go
+        import panel as pn
+        from bokeh.models.widgets.tables import NumberFormatter
         from IPython.display import display
         from ipywidgets import GridBox, Layout
+
+        pn.extension("tabulator")
 
         from ..vparameters.bifurcations import parametrize_bifurcations
 
         trees = self.to_av_trees(fundus_data)
-        widgets = []
+        views = []
+        tables = []
         bifuractions_data = []
+
+        def focus_on(id):
+            def callback(event):
+                coord = trees[id].nodes_coord()[bifuractions_data[id]["node"][event.row]]
+                views[id].goto(coord[::-1], scale=3)
+
+            return callback
+
         for i, tree in enumerate(trees):
             bifurcations = parametrize_bifurcations(tree)
             bifuractions_data.append(bifurcations)
@@ -82,38 +94,61 @@ class FundusAVSegToBiomarkers:
                 )
 
             # Draw the table
-            cols = list(bifurcations.columns)
-            for col in ("d0", "d1", "d2", "θ1", "θ2", "θ_branching", "θ_assymetry"):
-                bifurcations[col] = bifurcations[col].astype(int)
+            formatter = {}
+            for col in ("d0", "d1", "d2"):
+                formatter[col] = NumberFormatter(format="0")
+            for col in ("θ1", "θ2", "θ_branching", "θ_assymetry"):
+                formatter[col] = NumberFormatter(format="0°")
             for col in ("assymetry_ratio", "branching_coefficient"):
-                bifurcations[col] = bifurcations[col].astype(float).round(2)
-            table = go.FigureWidget()
-            table.add_table(
-                header=dict(values=cols),
-                cells=dict(values=[bifurcations[col] for col in cols]),
+                formatter[col] = NumberFormatter(format="0.00a")
+            bifurcations = bifurcations.rename(columns={"branch0": "B0", "branch1": "B1", "branch2": "B2"})
+            table = pn.widgets.Tabulator(
+                bifurcations,
+                show_index=False,
+                disabled=True,
+                layout="fit_data",
+                formatters=formatter,
+                editors={k: None for k in bifurcations.columns},
+                text_align={k: "center" for k in bifurcations.columns},
+                width=860,
+                height=300,
+                frozen_columns=["node", "B0"],
+                align="center",
             )
+            table.on_click(focus_on(i))
 
-            widgets.extend([table, view])
+            views.append(view)
+            tables.append(table)
 
-        display(
-            GridBox(
-                widgets,
-                layout=Layout(grid_template_columns="repeat(2, 1fr)", grid_template_rows="repeat(600px, 1fr)"),
-            )
+        display(GridBox(views, layout=Layout(grid_template_columns="repeat(2, 1fr)", grid_template_rows="400px")))
+        return (
+            pn.Row(tables[0], pn.Spacer(width=10), tables[1], height=400, scroll=True),
+            trees,
+            bifuractions_data,
         )
 
-        return trees, bifuractions_data
-
     def inspect_branches(self, fundus_data: FundusData):
-        import plotly.graph_objects as go
+        import panel as pn
+        from bokeh.models.widgets.tables import NumberFormatter
         from IPython.display import display
         from ipywidgets import GridBox, Layout
+
+        pn.extension("tabulator")
 
         from ..vparameters.branches import parametrize_branches
 
         trees = self.to_av_trees(fundus_data)
-        widgets = []
+        views = []
+        tables = []
         branch_infos = []
+
+        def focus_on(id):
+            def callback(event):
+                coord = trees[id].geometric_data().branch_midpoint(branch_infos[id]["branch"][event.row])
+                views[id].goto(coord[::-1], scale=2)
+
+            return callback
+
         for i, tree in enumerate(trees):
             branch_info = parametrize_branches(tree)
             branch_infos.append(branch_info)
@@ -123,33 +158,42 @@ class FundusAVSegToBiomarkers:
             view["graph"] = tree.jppype_layer(edge_map=True, boundaries=True, node_labels=True, edge_labels=True)
 
             # Draw the table
-            branch_info["calibre"] = (
-                branch_info["mean_calibre"].round(1).astype(str) + "±" + branch_info["std_calibre"].round(1).astype(str)
+            branch_info = branch_info.rename(
+                columns={
+                    "mean_calibre": "μCal",
+                    "std_calibre": "σCal",
+                    "mean_curvature": "μCurv",
+                    "n_curvatures_roots": "n_curv",
+                }
             )
-            branch_info.drop(columns=["std_calibre", "mean_calibre"], inplace=True)
-            for col in ("mean_curvature", "τHart", "τGrisan"):
-                branch_info[col] = branch_info[col].astype(float).apply("{:.2e}".format)
-
-            for col in ("n_curvatures_roots",):
-                branch_info[col] = branch_info[col].astype(int)
-
-            cols = list(branch_info.columns)
-            cols.remove("calibre")
-            cols.insert(1, "calibre")
-
-            table = go.FigureWidget()
-            table.add_table(
-                header=dict(values=cols),
-                cells=dict(values=[branch_info[col] for col in cols]),
+            formatter = {}
+            for col in ("μCal", "σCal", "μCurv", "τHart", "τGrisan"):
+                formatter[col] = NumberFormatter(format="0.00a")
+            for col in ("n_curv",):
+                formatter[col] = NumberFormatter(format="0")
+            for col in ("assymetry_ratio", "branching_coefficient"):
+                formatter[col] = NumberFormatter(format="0.00a")
+            table = pn.widgets.Tabulator(
+                branch_info,
+                show_index=False,
+                disabled=True,
+                layout="fit_data",
+                formatters=formatter,
+                editors={k: None for k in branch_info.columns},
+                text_align={k: "center" for k in branch_info.columns},
+                width=860,
+                height=300,
+                frozen_columns=["branch"],
+                align="center",
             )
+            table.on_click(focus_on(i))
 
-            widgets.extend([table, view])
+            views.append(view)
+            tables.append(table)
 
-        display(
-            GridBox(
-                widgets,
-                layout=Layout(grid_template_columns="repeat(2, 1fr)", grid_template_rows="repeat(600px, 1fr)"),
-            )
+        display(GridBox(views, layout=Layout(grid_template_columns="repeat(2, 1fr)", grid_template_rows="400px")))
+        return (
+            pn.Row(tables[0], pn.Spacer(width=10), tables[1], height=400, scroll=True),
+            trees,
+            branch_infos,
         )
-
-        return trees, branch_infos
