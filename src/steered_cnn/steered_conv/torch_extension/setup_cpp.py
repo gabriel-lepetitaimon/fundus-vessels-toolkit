5baaa483b1c7a315664a409080af1d3c42095846
Gabriel Lepetit-Aimon <gabriel.lepetit-aimon@polymtl.ca>
2025-03-20 13:40:58 -0400

    Remove torch extension from steered_cnn

--- src/steered_cnn/steered_conv/torch_extension/setup_cpp.py
+++
@@ -1,6 +0,0 @@
-import setuptools
-from torch.utils import cpp_extension
-
-setuptools.setup(name='steered_kbase_cpp',
-                 ext_modules=[cpp_extension.CppExtension('steered_kbase_cpp', ['steered_kbase_cpp.cpp'])],
-                 cmdclass={'build_ext': cpp_extension.BuildExtension})
