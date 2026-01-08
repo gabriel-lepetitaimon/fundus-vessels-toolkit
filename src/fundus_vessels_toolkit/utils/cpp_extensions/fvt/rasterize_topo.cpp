ccba04baaebe0cbfe5edfc3c678c4ba0fd4ec665
gabriel.lepetitaimon <gabriel.lepetitaimon@gmail.com>
2026-01-08 18:33:41 -0500

    ...

--- src/fundus_vessels_toolkit/utils/cpp_extensions/fvt/rasterize_topo.cpp
+++ src/fundus_vessels_toolkit/utils/cpp_extensions/fvt/rasterize_topo.cpp
@@ -177,7 +177,7 @@ void rasterize_topology(const torch::Tensor& branch_list, const torch::Tensor& b
                 } else {
                     // Otherwise draw bezier cubic interpolation
                     const IntPoint& node_yx = nodes_yx[branch.head_node];
-                    double d = distance(Point(headTip.yx), Point(childTip.yx));
+                    double d = distance(Point(headTip.yx), Point(childTip.yx)) * 0.5;
                     const Point c0 = headTip.yx + headTip.t * d;    // distance(Point(headTip.yx), Point(node_yx));
                     const Point c1 = childTip.yx - childTip.t * d;  // distance(Point(childTip.yx), Point(node_yx));
                     rasterize_bezier(drawJunctionTopo, {headTip.yx, c0, c1, childTip.yx}, headTip.b, childTip.b,
