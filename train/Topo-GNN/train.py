6d4d5eaf0ed33b90e90f4a490f2cd14597746173
Gabriel Lepetit-Aimon <gabriel.lepetit-aimon@polymtl.ca>
2026-06-08 18:17:52 -0400

    ...

--- train/Topo-GNN/train.py
+++ train/Topo-GNN/train.py
@@ -465,7 +465,7 @@ class DigraphGNNTrainer(L.LightningModule):
         self.test_metrics.reset()
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.config.lr)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config.lr / 25)
         # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=5)
         scheduler = torch.optim.lr_scheduler.OneCycleLR(
             optimizer, max_lr=self.config.lr, epochs=self.config.epoch, steps_per_epoch=self.n_step_per_epoch
